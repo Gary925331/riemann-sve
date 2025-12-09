d1155c2d99fb831ba28e637ce2c49f9baadd948c
Gary Lin <gary925331@alum.ccu.edu.tw>
2025-12-09 11:08:48 +0800

    Computing the DY/DX and update x,y

--- Euler/euler.py
+++ Euler/euler.py
@@ -1,4 +1,12 @@
 DX = 0.1
+X = 1
+Y = 1
 
 for i in range(5):
     print(f"Computing step {i}")
+    DY_DX = (Y*Y +2*X*Y)/X*X
+    print(f"DY_DX = {DY_DX}")
+    DY = DX*DY_DX
+    Y = Y + DY
+    X = X + DX
+    print(f"(X,Y) = {X}, {Y}")
