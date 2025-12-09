8a30a936032187a82c5f45d9aa2b24bb6f1abf4e
Gary Lin <gary925331@alum.ccu.edu.tw>
2025-12-09 11:36:01 +0800

    add graph

--- Euler/euler.py
+++ Euler/euler.py
@@ -1,3 +1,6 @@
+import matplotlib
+matplotlib.use('TkAgg') # Or 'Qt5Agg'
+import matplotlib.pyplot as plt
 DX = 0.01
 X = 1
 Y = 1
@@ -12,3 +15,9 @@ for i in range(50):
     print(f"(X,Y) = {X}, {Y}")
     REAL_Y = (1/2)*X*X/(1-0.5*X)
     print(f"REAL_Y = {REAL_Y}")
+
+plt.plot(X, Y)
+plt.xlabel('X-axis Label')
+plt.ylabel('Y-axis Label')
+plt.title('My Simple Line Graph')
+plt.show()
