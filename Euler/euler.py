beae29b2aa58bb0a63d29e33b608aab1a422f729
Gary Lin <gary925331@alum.ccu.edu.tw>
2025-12-09 11:43:22 +0800

    make a graph with line

--- Euler/euler.py
+++ Euler/euler.py
@@ -1,9 +1,15 @@
 import matplotlib
 matplotlib.use('TkAgg') # Or 'Qt5Agg'
 import matplotlib.pyplot as plt
+import array as arr
+
 DX = 0.01
 X = 1
 Y = 1
+
+X_ARRAY = arr.array('f', [1])
+Y_ARRAY = arr.array('f', [1])
+
 
 for i in range(50):
     print(f"Computing step {i}")
@@ -12,11 +18,13 @@ for i in range(50):
     DY = DX*DY_DX
     Y = Y + DY
     X = X + DX
+    X_ARRAY.append(X)
+    Y_ARRAY.append(Y)
     print(f"(X,Y) = {X}, {Y}")
     REAL_Y = (1/2)*X*X/(1-0.5*X)
     print(f"REAL_Y = {REAL_Y}")
 
-plt.plot(X, Y)
+plt.plot(X_ARRAY, Y_ARRAY)
 plt.xlabel('X-axis Label')
 plt.ylabel('Y-axis Label')
 plt.title('My Simple Line Graph')
