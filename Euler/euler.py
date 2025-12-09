e20089b12de92593a1a2d9b396129a521aed8ba3
Gary Lin <gary925331@alum.ccu.edu.tw>
2025-12-09 11:22:00 +0800

    solve the promblem of REAL_Y

--- Euler/euler.py
+++ Euler/euler.py
@@ -1,12 +1,14 @@
-DX = 0.1
+DX = 0.01
 X = 1
 Y = 1
 
-for i in range(5):
+for i in range(50):
     print(f"Computing step {i}")
-    DY_DX = (Y*Y +2*X*Y)/X*X
-    print(f"DY_DX = {DY_DX}")
+    DY_DX = (Y*Y +2*X*Y)/(X*X)
+    print(f"X,Y = ({X},{Y}), DY_DX = {DY_DX}")
     DY = DX*DY_DX
     Y = Y + DY
     X = X + DX
     print(f"(X,Y) = {X}, {Y}")
+    REAL_Y = (1/2)*X*X/(1-0.5*X)
+    print(f"REAL_Y = {REAL_Y}")
