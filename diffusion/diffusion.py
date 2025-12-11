18c447c1d27859ccb71d9c394d43f8b0cd3517b3
Gary Lin <gary925331@alum.ccu.edu.tw>
2025-12-11 21:02:15 +0800

    Can make a graph,but the answer is strange

--- diffusion/diffusion.py
+++ diffusion/diffusion.py
@@ -4,28 +4,41 @@ import matplotlib
 matplotlib.use('TkAgg') # Or 'Qt5Agg'
 import matplotlib.pyplot as plt
 
-L = 0.1
+L = 1
 NO_CELLS = 200
 U = 0.5
 ALPHA = 0.1
 DX = L / NO_CELLS
 T_END = 0.5
-DT = DX / ALPHA
-NO_TIME_STEPS = 500
+DT = DX / abs(ALPHA)
+NO_TIME_STEPS = 100
 NO_INTERFACES = NO_CELLS + 1
 
+x = np.linspace(0.0, 1.0 ,NO_CELLS)
 H_INIT = np.zeros(NO_CELLS)
 H = np.zeros(NO_CELLS)
+
 H_new = np.zeros(NO_CELLS)
 F = np.zeros(NO_INTERFACES)
 
 
+start = int(0.2 / DX)
+end = int(0.4 / DX)
+for i in range(start, end):
+    H[i] = 1.0
+
+print(f"Initial pulse in cells {start} to {end - 1}")
+
+
 H_INIT[:] = H[:]
+
+print(f"H = {H}")
 
 for step in range(NO_TIME_STEPS):
     print(f"Computing steps {step}")
 
     for i in range(1, NO_INTERFACES - 1):
+        #print("Computing flux at interface", i)
 
         F[i] = 0
 
@@ -36,18 +49,22 @@ for step in range(NO_TIME_STEPS):
         
         F[i] += -ALPHA*(H[i] - H[i - 1]) / DX
 
+        print(f"F = {F}")
+
     
     for i in range(NO_CELLS):
         H_new[i] = H[i] - (DT / DX) * (F[i+1] - F[i])
 
     H[:] = H_new[:]
 
-x = np.linspace(0.0, 1.0 ,NO_CELLS)
+    
+
+
 plt.plot(x, H)
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.legend()
 plt.show()
-plt.savefig("Euler_method_graph.png", dpi=1000, bbox_inches='tight')
+plt.savefig("diffusion_graph.png", dpi=1000, bbox_inches='tight')
 
 
