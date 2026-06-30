0755d7ff72b9dcca8db82c5608d27411058fb023
Gary Lin <gary925331@alum.ccu.edu.tw>
2026-06-30 13:16:46 +0800

    put the docker compose on the old code

---
+++ 1DAdvectionDockercompose/main.c
@@ -0,0 +1,192 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define N 100          /* number of cells */
+#define NIF (N+1)      /* number of interfaces */
+#define ALPHA 1.0    /* advection speed */
+#define L 1.0          /* domain length */
+#define DX (L / N)    /* cell size */
+#define CFL 0.05
+#define DT (CFL*DX / fabs(ALPHA)) /* time step size */   
+#define T_FINAL 0.2
+#define MAX_TIMESTEPS 50000
+
+
+void Compute_Fluxes(int nif, const double *u, double *F, double alpha)
+{
+    /* compute flux at all interfaces j = 0..n (n+1 interfaces)
+       interface j is between left = (j-1) and right = j (mod n) */
+
+    // Only compute the inside interfaces; leave the outside 2 interfaces for later.
+//    clock_t start = clock();   
+    for (int j = 1; j < (nif-1); j++) {
+        int left  = j - 1;
+        int right = j;
+        
+        double left_F = ALPHA*u[left];
+        double right_F = ALPHA*u[right];
+        double left_u = u[left];
+        double right_u = u[right];
+        
+        // Upwind scheme
+        
+        if (ALPHA > 0) {
+            F[j] = left_F;
+        } else {
+            F[j] = right_F;
+        }
+        
+
+       //central flux
+       // F[j] = 0.5 * (left_F + right_F);
+
+
+       // Rusanov flux
+       // F[j] = 0.5 * (left_F + right_F) - 0.25*(right_u - left_u);
+
+
+    }
+//    clock_t end = clock();
+//    double cpu_time = (double)(end - start) / CLOCKS_PER_SEC; // Convert to seconds
+
+//    printf("Execution time: %f seconds\n", cpu_time);
+    // Set dF/dx = 0 on left and right ends of our domain
+    F[0] = F[1];
+    F[nif-1] = F[nif-2];
+}
+
+
+void Update_State(int n, const double *F, double *u) {
+
+    /*
+    Solving
+        du/dt + dF/dx = 0
+    So
+        U* = U - dt*dF/dx
+    */
+    for (int cell = 0; cell < n; cell++) {
+        u[cell] = u[cell] - (DT/DX)*(F[cell+1] - F[cell]);
+    }
+
+}
+
+
+
+int main(void)
+{
+    clock_t start_t, end_t;
+    double t1,t2,t3,t4,t5;
+    start_t = clock(); 
+    double *u = malloc(sizeof(double) * N);
+    double *F = malloc(sizeof(double) * NIF);
+    double *A = malloc(sizeof(double) * N);
+    end_t = clock();
+    double memory_time = (double)(end_t - start_t) / CLOCKS_PER_SEC; // Convert to seconds
+    printf("setting memory time %f seconds\n",memory_time);
+
+    if (!u || !F) {
+        fprintf(stderr, "allocation failed\n");
+        return 1;
+    }
+
+    /* initial condition */
+    start_t = clock(); 
+    for (int i = 0; i < N; ++i) {
+        double x = (i + 0.5) * DX;
+        u[i] = 0.1;
+        if ((x > 0.2) && (x < 0.4)) {
+            u[i] = 0.5;
+        }
+    }
+    end_t = clock();
+    double initial_time = (double)(end_t - start_t) / CLOCKS_PER_SEC; // Convert to seconds
+    printf("setting initial condition time %f seconds\n",initial_time);
+
+
+    /* Compute the analytical solution first */
+    start_t = clock(); 
+    for (int i = 0; i < N; ++i) {
+        double x = (i + 0.5) * DX;
+        A[i] = 0.1;
+        if ((x > (0.2+ALPHA*T_FINAL)) && (x < 0.4+ALPHA*T_FINAL)) {
+            A[i] = 0.5;
+        }
+    }
+    end_t = clock();
+    double A_time = (double)(end_t - start_t) / CLOCKS_PER_SEC; // Convert to seconds
+    printf("setting A time %f seconds\n",A_time);
+
+
+
+    double time = 0.0;
+    start_t = clock();
+    for (int timestep = 0; timestep < MAX_TIMESTEPS; timestep++) {
+    
+        // Compute fluxes
+        Compute_Fluxes(NIF, u, F, ALPHA);
+
+        // Update U using Fluxes F
+//        printf("Computing state at time %g (using) timestep %g (after %d time steps)\n", time, DT, timestep);
+
+        Update_State(N, F, u);
+
+        time = time + DT;
+        if (time > T_FINAL) {
+//            printf("Arrived at target time; stopping.\n");
+            break;
+//        } else {
+//            printf("Ran out of timesteps before reaching target time.\n");
+        }
+
+    }
+    end_t = clock();
+    double compute_time = (double)(end_t - start_t) / CLOCKS_PER_SEC; // Convert to seconds
+    printf("compute time %f seconds\n",compute_time);
+
+
+    double Total_error = 0.0;
+    start_t = clock();
+    for (int i = 0; i < N; i++)
+    {
+        Total_error = Total_error + (u[i] - A[i])*(u[i] - A[i]);
+    }
+    // Find the average
+    end_t = clock();
+    double error_time = (double)(end_t - start_t) / CLOCKS_PER_SEC; // Convert to seconds
+    printf("total error time %f seconds\n",error_time);
+    Total_error = (float)(Total_error / N);
+    printf("Total error %g\n", Total_error); 
+
+    
+
+
+
+
+
+    /* write fluxes to file: one line per interface (index, flux) */
+    start_t = clock();
+    FILE *fp = fopen("fluxes.dat", "w");
+    for (int j = 0; j < NIF; ++j) {
+        fprintf(fp, "%d %.15e\n", j, F[j]);
+    }
+    fclose(fp);
+
+    // Write U to file now
+    fp = fopen("results.dat", "w");
+    for (int cell = 0; cell < N; cell++) {
+        double x = (cell+0.5)*DX;
+        fprintf(fp, "%g\t%g\n", x, u[cell]);
+    }
+    fclose(fp);
+    end_t = clock();
+    double save_time = (double)(end_t - start_t) / CLOCKS_PER_SEC; // Convert to seconds
+    printf("save time %f seconds\n",save_time);
+    /* cleanup */
+    free(u);
+    free(F);
+    free(A);
+
+    return 0;
+}
