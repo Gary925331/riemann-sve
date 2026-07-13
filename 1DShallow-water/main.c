56e01d983863f3e1c539324ee9492a63e6132b56
Gary Lin <gary925331@alum.ccu.edu.tw>
2026-07-13 17:17:01 +0800

    update the mass and momentum,then save the results

--- 1DShallow-water/main.c
+++ 1DShallow-water/main.c
@@ -7,10 +7,10 @@
 #define ALPHA 1.0    /* advection speed */
 #define L 1.0          /* domain length */
 #define DX (L / N)    /* cell size */
-#define MAX_TIMESTEPS 5000
-#define T_FINAL 0.1
+#define MAX_TIMESTEPS 50000
+#define T_FINAL 1
 #define g 9.81
-#define CFL 0.5
+#define CFL 0.25
 
 void Allocate_memory(float **u,float **mass_F,float **momentum_F,float **mass,float **momentum,float **h){
 	*u = (float*)malloc(N*sizeof(float));
@@ -31,9 +31,9 @@ void Free_memory(float *u,float *mass_F,float *momentum_F,float *mass,float *mom
 void Calcaulation(float *u,float *mass_F,float *momentum_F,float *mass,float *momentum,float *h){
 	for (int i = 0;i < N;i++){
 		if(i < N/2){
-			h[i] = 10;
-		}else{
 			h[i] = 1;
+		}else{
+			h[i] = 0.1;
 		}
 		printf("h[%d]=%f\n",i,h[i]);
 		u[i] = 0;
@@ -41,23 +41,25 @@ void Calcaulation(float *u,float *mass_F,float *momentum_F,float *mass,float *mo
 		momentum[i] = h[i]*u[i];
 	}
 	float time = 0;
-        float Smax;
+        
 	for (int timestep = 0; timestep < MAX_TIMESTEPS; timestep++){
+		float Smax = 0.0;
 		for (int i = 1;i < NIF-1;i++){
 			float S_L = u[i-1] + sqrt(g*h[i-1]);
 			float S_R = u[i] + sqrt(g*h[i]);
 			float S_local_max;
 			if (S_L > S_R){
-				S_L = S_local_max;
+				S_local_max = S_L;
 			}else{
-				S_R = S_local_max;
+				S_local_max = S_R;
 			}
 			if(S_local_max > Smax){
 				Smax = S_local_max;
 			}
 		}
 		float DT = CFL*DX/Smax;
-		time = time + DT;
+		printf("%f",DT);
+//		time = time + DT;
         	if (time > T_FINAL) {
             		printf("Arrived at target time; stopping.\n");
             		break;
@@ -69,16 +71,23 @@ void Calcaulation(float *u,float *mass_F,float *momentum_F,float *mass,float *mo
 			float mass_right = h[i]*u[i];
 			float momentum_left = h[i-1]*u[i-1]*u[i-1] + 0.5*g*h[i-1]*h[i-1];
 			float momentum_right = h[i]*u[i]*u[i] + 0.5*g*h[i]*h[i];
-                        mass_F[i] = 0.5*(mass_left + mass_right) + 0.5*Smax*(mass[i] - mass[i-1]);
-			momentum_F[i] = 0.5*(momentum_left + momentum_right) +0.5*Smax*(momentum[i] - momentum[i-1]);
+                        mass_F[i] = 0.5*(mass_left + mass_right) - 0.5*Smax*(mass[i] - mass[i-1]);
+			momentum_F[i] = 0.5*(momentum_left + momentum_right) - 0.5*Smax*(momentum[i] - momentum[i-1]);
 			mass_F[0] = mass_F[1];
 			mass_F[NIF-1] = mass_F[NIF-2];
 			momentum_F[0] = momentum_F[1];
-			momentum_F[NIF-1] = momentum[NIF-2];
+			momentum_F[NIF-1] = momentum_F[NIF-2];
 		}
-		for(int i = 1;i < N;i++){
+		for(int i = 0;i < N;i++){
+			mass[i] = mass[i] - DT*(mass_F[i+1]-mass_F[i])/DX;
+			momentum[i] = momentum[i] - DT*(momentum_F[i+1]-momentum_F[i])/DX;
+			printf("%f,%f\n",mass[i],momentum[i]); 
 		}
-
+		for(int i = 0;i < N;i++){
+			h[i] = mass[i];
+			u[i] = momentum[i]/mass[i];
+		}
+		time = time + DT;
 
 	}
 }
@@ -91,6 +100,12 @@ int main() {
         float *h;
         Allocate_memory(&u,&mass_F,&momentum_F,&mass,&momentum,&h);
 	Calcaulation(u,mass_F,momentum_F,mass,momentum,h);
+	FILE *fp = fopen("results.dat", "w");
+    	for (int j = 0; j < N; j++) {
+		float X = (j+0.5)*DX;
+        	fprintf(fp, "%g\t%.15e\t%g\n", X, mass[j],momentum[j]);
+    	}
+    	fclose(fp);
         Free_memory(u,mass_F,momentum_F,mass,momentum,h);
 }
 
