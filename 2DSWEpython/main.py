bffa36ebb4bc126b742228f4b8bdb6d4b022f22b
Gary Lin <gary925331@alum.ccu.edu.tw>
2026-09-08 16:31:19 +0800

    Set the array

--- 2DSWEpython/main.py
+++ 2DSWEpython/main.py
@@ -17,5 +17,25 @@ T_FINAL = 7.2
 g = 9.81
 CFL = 0.1
 
+u = np.zeros(N)
+v = np.zeros(N)
+s = np.zeros(N)
+mass_F = np.zeros(NIF)
+momentum_F_X = np.zeros(NIF)
+momentum_F_Y = np.zeros(NIF)
+mass_G = np.zeros(NIF)
+momentum_G_X = np.zeros(NIF)
+momentum_G_Y = np.zeros(NIF)
+mass = np.zeros(NIF)
+momentum_X = np.zeros(NIF)
+momentum_Y = np.zeros(NIF)
+h = np.zeros(N)
+h_slope_X = np.zeros(N)
+u_slope_X_X = np.zeros(N)
+v_slope_X_Y = np.zeros(N)
+h_slope_Y = np.zeros(N)
+u_slope_Y_X = np.zeros(N)
+v_slope_Y_Y = np.zeros(N)
+
 print("Hello world")
 
